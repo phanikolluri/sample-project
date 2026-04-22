29b94abe26072333e7f89fa8d9ffad6c5125bcfe
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-04-22 15:48:36 +0530

    pushing terraform project with ansible with load balancing setup

---
+++ main.tf
@@ -0,0 +1,26 @@
+module "databases" {
+  for_each = var.databases
+  source = "./modules/component"
+
+  dns_domain = var.dns_domain
+  component = each.key
+  env = var.env
+  instance_type = each.value["instance_type"]
+  ports =  each.value["ports"]
+}
+
+
+module "apps" {
+  for_each = var.apps
+  source = "./modules/component"
+  
+  dns_domain = var.dns_domain
+  component = each.key
+  env = var.env
+  instance_type = each.value["instance_type"]
+  ports =  each.value["ports"]
+}
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "env" {}
+variable "dns_domain" {}
+variable "databases" {}
+
+variable "apps" {}
+
+
