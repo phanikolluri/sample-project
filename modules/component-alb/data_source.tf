5637ed7adf3ceca72405b9c2aa0259dd8a9adc3a
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-04-22 15:37:18 +0530

    pushing terraform project with ansible with load balancing setup

---
+++ modules/component-alb/data_source.tf
@@ -0,0 +1,15 @@
+data "aws_route53_zone" "main" {
+  name         = var.dns_domain
+}
+
+
+data "aws_ami" "ami" {
+
+  owners = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
+}
+
---
+++ modules/component-alb/variables.tf
@@ -0,0 +1,16 @@
+variable "component" {}
+variable "env" {}
+variable "instance_type" {}
+variable "dns_domain" {}
+variable "ports" {}
+variable "subnets" {}
+variable "lb" {}
+variable "asg_min_size" {}
+variable "asg_max_size" {}
+
+
+
+
+
+
+
